310b1c83c980bfc2dc619f5d83334f59f204ffb4
Jordan D <jordan.delrosario@gmail.com>
2021-03-18 12:05:18 -0400

    Updated Diagnostic Procedure

--- server/models/DiagnosticProcedureID.js
+++ server/models/DiagnosticProcedureID.js
@@ -3,7 +3,6 @@ var Schema = mongoose.Schema;
 
 var DiagnosticProcedureID = new Schema({
     id: {type: String, required: true},
-    responses: [Number], //TODO: replace with SDCQuestionResponse?,
     });
 
 module.exports = mongoose.model("DiagnosticProcedureID", DiagnosticProcedureID);
