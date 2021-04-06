7c4505bce86dd9050ed01edac860246dbd80b91d
Jordan <delrosario.jordan.r@gmail.com>
2021-04-06 12:06:08 -0400

    deploying to heroku

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 async function startServer() {
   let app = express();
-
+  port = process.env.PORT || config.port;
   await loaders({ expressApp: app });
 
   /** Import the various routes **/
@@ -12,12 +12,14 @@ async function startServer() {
   app.use(bodyParser.json());
   app.use(require("./routes/patients"));
 
-  app.listen(config.port, (err) => {
+  app.use(express.static('client/build'))
+
+  app.listen(port, (err) => {
     if (err) {
       console.log(err);
       return;
     }
-    console.log(`Your server is ready! At port: ` + config.port);
+    console.log(`Your server is ready! At port: ` + port);
   });
 }
 
